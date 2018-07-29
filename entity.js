54155b23e4ea2a1362d9308fd8e6b9858ff1999e
ksoo <hsnks100@gmail.com>
2018-07-30 00:43:03 +0900

    꼬리 달기

--- entity.js
+++ entity.js
@@ -16,10 +16,12 @@ var bulletGenInterval = 15;
 
 function preload() { 
     game.load.image('wall', 'wall.png'); 
+    game.load.image('tail', 'tail_bullet.png'); 
+    game.load.image('player', 'player.png'); 
 }
 
 function createBullet() { 
-    var s = game.add.sprite(0, 0, 'wall'); 
+    var s = game.add.sprite(0, 0, 'tail'); 
     s.anchor.x = 0.5;
     s.anchor.y = 0.5;
     s.custom = {};
@@ -36,6 +38,7 @@ function createBullet() {
     var dy = playerSprite.position.y - s.position.y; 
     s.custom.angle = Math.atan2(dy, dx); 
     s.custom.speed = 1 + Math.random() * 2;
+    s.custom.aim = true;
     bulletSprites.push(s); 
 }
 
@@ -57,6 +60,22 @@ function collisionDetect() {
 function moveBullets() {
     for(var bi in bulletSprites) {
         var b = bulletSprites[bi];
+        var yDiff = (b.position.y - playerSprite.position.y);
+        yDiff *= yDiff;
+
+        var xDiff = (b.position.x - playerSprite.position.x);
+        xDiff *= xDiff; 
+        var distance = yDiff + xDiff; 
+        var dx = playerSprite.position.x - b.position.x;
+        var dy = playerSprite.position.y - b.position.y; 
+        if(b.custom.aim) {
+            if(distance < 30000) {
+                console.log("near!");
+                b.custom.aim = false;
+                b.custom.angle = Math.atan2(dy, dx); 
+            }
+        }
+        b.angle = (b.custom.angle) * 180 / Math.PI + 90;
         b.position.x += b.custom.speed * Math.cos(b.custom.angle);
         b.position.y += b.custom.speed * Math.sin(b.custom.angle);
     }
@@ -89,14 +108,14 @@ function update() {
     moveBullets(); 
     collisionDetect(); 
     generator(); 
-    console.log(frameNumber); 
+    //console.log(frameNumber); 
 
 }
 
 function create() { 
-    playerSprite = game.add.sprite(0, 0, 'wall'); 
-    playerSprite.scale.x = 0.5; 
-    playerSprite.scale.y = 0.5; 
+    playerSprite = game.add.sprite(0, 0, 'player'); 
+    playerSprite.scale.x = 0.2; 
+    playerSprite.scale.y = 0.2; 
     playerSprite.position.y = 32 * 19; 
     playerSprite.position.x = 32; 
     playerSprite.anchor.x = 0.5; 
@@ -106,8 +125,8 @@ function create() {
     leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
     rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
     
-    for(var i=0; i<10; i++) {
-        createBullet();
+    for(var i=0; i<1; i++) {
+        //createBullet();
     }
 } 
 
